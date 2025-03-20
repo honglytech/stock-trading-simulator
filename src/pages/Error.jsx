566c76f9f2625aaad1e8eb1230231cbfa860b870
Hong Ly <lyhong.rupp@outlook.com>
2025-03-20 20:21:17 +1100

    setup error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,14 @@
-export const Error = () => {
-  return <h1>Error Page</h1>;
+import { Link, useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <Link to="/dashboard">back home</Link>
+    </div>
+  );
 };
 
 export default Error;
