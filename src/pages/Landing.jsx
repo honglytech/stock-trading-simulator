897879d40862717bd0d4b841d913ff37ddba4017
Hong Ly <lyhong.rupp@outlook.com>
2025-03-24 20:04:12 +1100

    install styled components

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,21 @@
+import styled from "styled-components";
+
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <div>
+      <Button>Click me</Button>
+      <h1>Landing Page</h1>
+    </div>
+  );
 };
+
+const Button = styled.button`
+  background-color: #007bff;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  padding: 8px 16px;
+`;
 export default Landing;
