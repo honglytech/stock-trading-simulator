0f4ef1c39baa023df96cd111956d7c3d367d9851
Hong Ly <lyhong.rupp@outlook.com>
2025-03-27 21:17:28 +1100

    update landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,33 @@
 import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div>
-      <Button>Click me</Button>
-      <h1>Landing Page</h1>
-    </div>
+    <Wrapper>
+      <nav>
+        <h1>Logo</h1>
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Stock Trading <span>Simulator</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae
+            adipisci architecto numquam ad eaque commodi alias nihil voluptate
+            omnis voluptates itaque soluta, fugit veniam quos, officia quasi
+            assumenda suscipit! Deserunt?
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login
+          </Link>
+        </div>
+      </div>
+    </Wrapper>
   );
 };
 
